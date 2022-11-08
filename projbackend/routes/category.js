c46adcf73561d3ea6d7e2abbd51ab1966a50ba37
Venkatesh <venkateshkoppar@gmail.com>
2022-11-08 09:47:50 +0530

    created add new category

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express();
 
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategory } = require("../controllers/category");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
+//params
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
+
+//actual routes goes here
+router.post(
+  "/category/createroute/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
 
 module.exports = router;

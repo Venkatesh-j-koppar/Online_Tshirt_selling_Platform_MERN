f7b455a19f344e791430023d9da50df20127a7e9
Venkatesh <venkateshkoppar@gmail.com>
2022-11-25 21:24:49 +0530

    Front End changes

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles.css";
+import { API } from "../user/backend";
+import Base from "./Base";
 
 export default function Home() {
+  console.log("Hello");
+  console.log("API IS", { API });
   return (
-    <div>
-      <h1 className="text-white">Hello front End</h1>
-    </div>
+    <Base>
+      <h1 className="text-white">Hello front End {API}</h1>
+    </Base>
   );
 }

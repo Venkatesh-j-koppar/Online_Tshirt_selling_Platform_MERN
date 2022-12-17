cc21e022780b0f13dda9caca5ac6ce7d80a84432
Venkatesh <venkateshkoppar@gmail.com>
2022-12-17 23:16:27 +0530

    changes in signin

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,8 +1,59 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { signin, authenticate, isAutheticated } from "../auth/helper";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAutheticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const successMessage = () => {
+    return (
+      <div
+        className="alert alert-success"
+        style={{ display: success ? "" : "none" }}
+      >
+        New account was created succssfully. Please{" "}
+        <Link to="/signin">Login Here</Link>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: false, loading: true });
+        }
+      })
+      .catch();
+  };
+
   const signInForm = () => {
     return (
       <div className="row">
@@ -10,13 +61,28 @@ const Signin = () => {
           <form>
             <div className="form-group my-2">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email"></input>
+              <input
+                value={email}
+                onChange={handleChange("email")}
+                className="form-control"
+                type="email"
+              ></input>
             </div>
             <div className="form-group my-2">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password"></input>
+              <input
+                className="form-control"
+                type="password"
+                value={password}
+                onChange={handleChange("password")}
+              ></input>
             </div>
-            <button className="btn btn-success btn-block my-2">Submit</button>
+            <button
+              className="btn btn-success btn-block my-2"
+              onClick={onSubmit}
+            >
+              Submit
+            </button>
           </form>
         </div>
       </div>
